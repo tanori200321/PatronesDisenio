3e06ec8ef98f02034499a21b65c7b1279e3dd7b1
Bryan Jesus Robles Mardueño <roblesmardueno@gmail.com>
2023-10-07 11:49:30 -0700

    Agregando Patron Strategy

---
+++ Strategy/Producto.cs
@@ -0,0 +1,22 @@
+﻿namespace Strategy
+{
+    public class Producto
+    {
+        private readonly IProductoDescuentoStrategy _strategy;
+
+        public Producto(IProductoDescuentoStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Descuento { get => CalcularDescuento(); }
+        public decimal PrecioDeVenta { get => Precio - CalcularDescuento(); }
+
+        public decimal CalcularDescuento() 
+        {
+            return _strategy.CalcularDescuento(Precio);
+        }
+    }
+}
